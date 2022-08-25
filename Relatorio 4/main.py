1a559cc48310de0490bd40118d349bcd35117dd0
lucasdemartha <lucasribm@gmail.com>
2022-08-25 08:57:59 -0300

    rel 4

---
+++ Relatorio 4/main.py
@@ -0,0 +1,12 @@
+from db.database import Database
+from helper.WriteAJson import writeAJson
+from dataset.produto_database import dataset
+
+compras = Database(database="database", collection="produtos", dataset=dataset)
+compras.resetDatabase()
+
+
+
+
+
+
